53fad6c1cfbc6f2d0c764b51f4eff5a7caa8989e
monk-55 <liv.tomkins@gmail.com>
2025-09-23 16:49:18 +0200

    Adding taskone.py that calls the functions from the module

---
+++ module04_b/taskone.py
@@ -0,0 +1,16 @@
+"""
+Module4 Task one - code to use the functions in the module created
+"""
+#importing the module already created
+import MyModule
+
+#The next 3 lines use each of the functions in Mymodule
+numbers = MyModule.get_a_list_of_numbers()
+
+numbers_min = MyModule.find_min(numbers)
+numbers_max = MyModule.find_max(numbers)
+
+
+print(f'The list of number is: {numbers}')    
+print(f'The minimal number is: {numbers_min}')
+print(f'The maximal number is: {numbers_max}')
