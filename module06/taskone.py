b6678b8ce7479293a9afdd79d10aa947ac3b45ce
monk-55 <liv.tomkins@gmail.com>
2025-10-06 11:12:00 +0200

    Updated chart type to a pie chart

--- module06/taskone.py
+++ module06/taskone.py
@@ -1,5 +1,7 @@
 """
 module06 task 1
+First plot simple bar chart
+Second plot pie chart
 """
 
 import matplotlib.pyplot as plt
@@ -9,10 +11,19 @@ Y = [37.7, 20.4, 19.5, 10.6, 11.8]
 
 fig, ax = plt.subplots()
 
-ax.set_xlabel('Energy source')
-ax.set_ylabel('Percentage of total energy generation (%)')
+"""
+code commented out is for a bar chart
+"""
+# ax.set_xlabel('Energy source')
+# ax.set_ylabel('Percentage of total energy generation (%)')
 
-ax.set_title('Percentage shares of energy generation within the EU in 2023')
+# ax.set_title('Percentage shares of energy generation within the EU in 2023')
 
-ax.bar(X,Y, color='blue')
+# ax.bar(X,Y, color='blue')
+
+
+""" the code line below is specific for a pie chart
+plt.show() is used for both examples"""
+wedges, texts, autotexts = ax.pie(Y, labels=X, autopct='%1.1f%%', startangle=90) 
+
 plt.show()
